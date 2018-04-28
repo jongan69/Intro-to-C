46cc3ac49664b340af5a0552f0fe63b0b24555a9
jongan69 <jonathang132298@gmail.com>
2018-04-28 15:25:50 -0400

    y

---
+++ studying.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int pp()
+{
+
+int x = 10;
+
+printf("enetr a number: ");
+scanf("%d",&x);
+
+int array[5] = {3,4,5,6,9};
+
+int* point = (int*)malloc(5);
+}
+
+
+
+int main ()
+{
+
+
+
+return 0;
+}
+
+
